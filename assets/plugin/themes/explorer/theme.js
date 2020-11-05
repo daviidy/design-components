4dc60dcea5d30df597844b5f99aa8c6f2f1738fa
aouman <k2.aouman@gmail.com>
2020-11-05 08:04:04 +0000

    correction du linter

--- assets/plugin/themes/explorer/theme.js
+++ assets/plugin/themes/explorer/theme.js
@@ -10,31 +10,55 @@
  * Licensed under the BSD-3-Clause
  * https://github.com/kartik-v/bootstrap-fileinput/blob/master/LICENSE.md
  */
-(function ($) {
-    "use strict";
-    $.fn.fileinputThemes.explorer = {
-        layoutTemplates: {
-            footer: '<div class="file-details-cell">' +
-                '<div class="explorer-caption" title="{caption}">{caption}</div> ' + '{size}{progress}' +
-                '</div>' +
-                '<div class="file-actions-cell">{indicator} {actions}</div>',
-            actions: '{drag}\n' +
-                '<div class="file-actions">\n' +
-                '    <div class="file-footer-buttons">\n' +
-                '        {upload} {download} {delete} {zoom} {other} ' +
-                '    </div>\n' +
-                '</div>',
-        },
-        previewSettings: {
-            html: {width: "100px", height: "60px"},
-            text: {width: "100px", height: "60px"},
-            video: {width: "auto", height: "60px"},
-            audio: {width: "auto", height: "60px"},
-            flash: {width: "100%", height: "60px"},
-            object: {width: "100%", height: "60px"},
-            pdf: {width: "100px", height: "60px"},
-            other: {width: "100%", height: "60px"}
-        },
-        frameClass: 'explorer-frame'
-    };
+(function($) {
+  "use strict";
+  $.fn.fileinputThemes.explorer = {
+    layoutTemplates: {
+      footer: '<div class="file-details-cell">' +
+        '<div class="explorer-caption" title="{caption}">{caption}</div> ' + '{size}{progress}' +
+        '</div>' +
+        '<div class="file-actions-cell">{indicator} {actions}</div>',
+      actions: '{drag}\n' +
+        '<div class="file-actions">\n' +
+        '    <div class="file-footer-buttons">\n' +
+        '        {upload} {download} {delete} {zoom} {other} ' +
+        '    </div>\n' +
+        '</div>',
+    },
+    previewSettings: {
+      html: {
+        width: "100px",
+        height: "60px"
+      },
+      text: {
+        width: "100px",
+        height: "60px"
+      },
+      video: {
+        width: "auto",
+        height: "60px"
+      },
+      audio: {
+        width: "auto",
+        height: "60px"
+      },
+      flash: {
+        width: "100%",
+        height: "60px"
+      },
+      object: {
+        width: "100%",
+        height: "60px"
+      },
+      pdf: {
+        width: "100px",
+        height: "60px"
+      },
+      other: {
+        width: "100%",
+        height: "60px"
+      }
+    },
+    frameClass: 'explorer-frame'
+  };
 })(window.jQuery);
